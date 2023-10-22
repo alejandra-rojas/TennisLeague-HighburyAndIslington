4a647d032d5e88eaf982b68836a03c5bef7c3913
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-10-22 11:14:57 +0200

    Delete Player component

---
+++ app/(admin)/admin/players/DeleteButton.jsx
@@ -0,0 +1,68 @@
+"use client";
+import { startTransition, useState } from "react";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { TiDelete } from "react-icons/ti";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { deletePlayer } from "./actions";
+
+export default function DeleteButton({ id }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    //console.log("deleting id", id);
+    const res = await fetch(`http://localhost:3000/api/players/${id}`, {
+      method: "DELETE",
+    });
+    const json = await res.json();
+
+    if (json.error) {
+      console.log(json.error);
+      setIsLoading(false);
+    }
+    if (!json.error) {
+      router.refresh();
+      // router.push("/admin");
+    }
+  };
+
+  return (
+    <button onClick={handleClick} disabled={isLoading}>
+      {isLoading && (
+        <>
+          <TiDelete />
+          Deleting...
+        </>
+      )}
+      {!isLoading && (
+        <>
+          <TiDelete />
+          Delete Ticket
+        </>
+      )}
+    </button>
+  );
+}
+
+/* export default function DeleteButton({ id }) {
+  const [isPending, startTransition] = useTransition(false);
+
+  return (
+    <button onClick={() => startTransition(() => deletePlayer(id))}>
+      {isPending && (
+        <>
+          <TiDelete />
+          Deleting...
+        </>
+      )}
+      {!isPending && (
+        <>
+          <TiDelete />
+          Delete Ticket
+        </>
+      )}
+    </button>
+  );
+} */
