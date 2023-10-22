ff284365fb52cb88017de7e3eb202296abb0eaaa
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-10-22 13:10:37 +0200

    Adding Vercel Web Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import ClientNavbar from "./components/ClientNavbar";
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
