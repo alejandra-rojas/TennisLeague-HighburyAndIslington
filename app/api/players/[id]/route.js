fa8ce0a276da29092631ed7303987f0e0abb89df
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-10-22 11:13:34 +0200

    Route - DeletePlayer

---
+++ app/api/players/[id]/route.js
@@ -0,0 +1,15 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function DELETE(_, { params }) {
+  const id = params.id;
+  //get supabase instance
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+
+  //insert data
+  const { error } = await supabase.from("players").delete().eq("player_id", id);
+
+  return NextResponse.json({ error });
+}
