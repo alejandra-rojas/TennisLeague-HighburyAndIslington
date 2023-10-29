dc8369ce6ae643e4142a25915e581d08afd6cba4
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-10-29 18:11:25 +0100

    GET endpoint

--- app/api/players/route.js
+++ app/api/players/route.js
@@ -22,3 +22,25 @@ export async function POST(request) {
 
   return NextResponse.json({ data, error });
 }
+
+//GET PLAYERS ENDPOINT FOR TEAMS/SEARCH COMPONENT -- CURRENTLY UNUSED AS USING CREATECLIENTCOMPONENTCLIENT
+export async function GET() {
+  // Initialize Supabase client
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+
+  // Fetch all players
+  const { data, error } = await supabase.from("players").select("*");
+
+  // Return the data or error
+  if (error) {
+    return new NextResponse(JSON.stringify({ error }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return NextResponse.json({ data });
+}
