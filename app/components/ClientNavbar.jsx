feb4ec0def40d53f7b98f8a0e8e383779745ad66
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-05 20:39:31 +0100

    Header Redesign

--- app/components/ClientNavbar.jsx
+++ app/components/ClientNavbar.jsx
@@ -2,44 +2,47 @@ import "../styles/Root.scss";
 import Link from "next/link";
 
 export default function ClientNavbar() {
-  return (
-    <>
-      <header id="web-header">
-        <div id="primary-navigation">
-          <div>
-            <Link href="/" aria-label="Go to the Home page">
-              <h1>Highbury Fields Doubles Leagues </h1>
-            </Link>
-          </div>
-          <div>
-            <nav role="navigation" aria-labelledby="primary-navigation">
-              <h2 id="primary-site-navigation" className="sr-only">
-                Home Site Navigation
-              </h2>
-              <ul>
-                <li>
-                  <Link
-                    href={"/report-results"}
-                    aria-label="Go to the Home page"
-                  >
-                    Report Results
-                  </Link>
-                </li>
-                <li>
-                  <Link href={"/rules"} aria-label="Go to About page">
-                    Rules
-                  </Link>
-                </li>
-                <li>
-                  <Link href={"/past-leagues"} aria-label="Go to Admin page">
-                    Past Leagues
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+	return (
+		<>
+			<header>
+				<div id="primary-navigation">
+					<div className="header-item">
+						<nav role="navigation" aria-labelledby="primary-navigation">
+							<h2 id="primary-site-navigation" className="sr-only">
+								Home Site Navigation
+							</h2>
+							<ul>
+								<li>
+									<Link
+										href={"/report-results"}
+										aria-label="Go to the Home page"
+									>
+										Report Results
+									</Link>
+								</li>
+								<li>
+									<Link href={"/rules"} aria-label="Go to About page">
+										Rules
+									</Link>
+								</li>
+								<li>
+									<Link href={"/past-leagues"} aria-label="Go to Admin page">
+										Past Leagues
+									</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div className="header-item">
+						<Link href="/" aria-label="Go to the Home page">
+							<h1>Highbury Doubles</h1>
+						</Link>
+					</div>
+					<div className="header-item">
+						<p>ball</p>
+					</div>
+				</div>
+			</header>
+		</>
+	);
 }
