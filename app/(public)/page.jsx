ecf91f73928955f5e1f79e01c3880a6386d41917
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-06 16:41:32 +0100

    court-hero

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -1,3 +1,20 @@
+import "../styles/Public/styles.scss";
+
 export default function Home() {
-  return <main>This is the home page of Highbury Fields Tennis Leagues</main>;
+	return (
+		<main>
+			<div class="court">
+				<div class="div1"> </div>
+				<div class="div2"> </div>
+				<div class="div3"> </div>
+				<div class="div4"> </div>
+				<div class="div5"> </div>
+				<div class="div6"> </div>
+				<div class="div7"> </div>
+				<div class="div8"> </div>
+				<div class="div9"> </div>
+				<div class="div10"> </div>
+			</div>
+		</main>
+	);
 }
