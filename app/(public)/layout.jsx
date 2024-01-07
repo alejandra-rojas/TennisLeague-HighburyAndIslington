42527c5275db336e4df57e4289f5aee62252006b
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-07 16:07:52 +0100

    Installed Lenis

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,14 +1,19 @@
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 //Components
 import ClientNavbar from "@/app/components/ClientNavbar";
 import "../styles/Public/styles.scss";
 
 export default async function ClientLayout({ children }) {
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
+
   return (
-    <>
+    <ReactLenis root>
       <header id="web-header">
         <ClientNavbar />
       </header>
       {children}
-    </>
+    </ReactLenis>
   );
 }
