32e2c8a98621bff184d6b202a4a19034c9087d5c
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-08 16:50:20 +0100

    Fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,21 @@
 import { Analytics } from "@vercel/analytics/react";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans, Open_Sans } from "next/font/google";
 import "./globals.scss";
 import TanstackProvider from "./components/providers/Tanstackprovider";
 import SmoothScroll from "./components/providers/SmoothScroll";
 
 export const dynamic = "force-dynamic";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const noto = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["200", "400", "700", "900"],
+  variable: "--font-noto",
+});
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-opensans",
+});
 
 export const metadata = {
   title: "Highbury Fields Tennis Leagues",
@@ -16,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={noto.variable}>
         <TanstackProvider>
           <SmoothScroll>{children}</SmoothScroll>
         </TanstackProvider>
@@ -25,3 +34,5 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
+//<body className={openSans.className}>
