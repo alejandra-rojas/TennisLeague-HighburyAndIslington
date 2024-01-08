1bf4fc706bde9674d6f7f28113a9a7705d8d797f
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-08 16:51:30 +0100

    Lenis

--- app/components/providers/SmoothScroll.jsx
+++ app/components/providers/SmoothScroll.jsx
@@ -2,12 +2,16 @@
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 
 function SmoothScroll({ children }) {
-  const lenis = useLenis(({ scroll }) => {
-    // called every scroll
-    console.log("lenis");
-  });
+  // const lenis = useLenis(({ scroll }) => {
+  //   // called every scroll
+  //   console.log("lenis", lenis.velocity);
+  // });
 
-  return <ReactLenis root>{children}</ReactLenis>;
+  return (
+    <ReactLenis root options={{ lerp: 0.1, smoothTouch: true }}>
+      {children}
+    </ReactLenis>
+  );
 }
 
 export default SmoothScroll;
