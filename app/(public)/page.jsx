ecc4cbe82f2977c5c0d83a656c8d70538a41a728
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-09 19:02:24 +0100

    Court Hero

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -5,6 +5,7 @@ import RotatingText from "../components/RotatingText";
 import "../styles/Public/styles.scss";
 import { useLenis } from "@studio-freight/react-lenis";
 import BallElement from "../components/BallElement";
+import CourtHero from "../components/CourtHero";
 
 export default function Home() {
   //   const lenis = useLenis((scroll) => {
@@ -13,40 +14,12 @@ export default function Home() {
 
   return (
     <main className="main-layout-client">
-      <div className="court">
-        <div className="details ">
-          <div className="text-container">
-            <div className="title">
-              Highbury Tennis Club runs a thriving doubles league for its women
-              members
-            </div>
-            <div className="subtitle">
-              The league is designed to give match practice for external LTA
-              competitions and to build confidence generally for all competitive
-              play.
-            </div>
-            <div className="buttons-container">
-              <div className="button accent">Find out more</div>
-              <div className="button">Latest results</div>
-            </div>
-          </div>
-        </div>
-        <div className="div1"> </div>
-        <div className="div2"> </div>
-        <div className="div3"> </div>
-        <div className="div4"> </div>
-        <div className="div5"> </div>
-        <div className="div6"> </div>
-        <div className="div7"> </div>
-        <div className="div8"> </div>
-        <div className="div9"> </div>
-        <div className="div10"> </div>
-      </div>
+      <CourtHero />
+      <div className="image-hero"></div>
 
+      <div className="end">a</div>
       {/* <TextRotating /> */}
       {/* <RotatingText /> */}
-
-      <div className="end"></div>
     </main>
   );
 }
---
+++ app/components/CourtHero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/Public/court.scss";
+
+function CourtHero() {
+  return (
+    <div className="court">
+      <div className="details ">
+        <div className="text-container">
+          <div className="title">
+            Highbury Tennis Club runs a thriving doubles league for its women
+            members
+          </div>
+          <div className="subtitle">
+            There are two club leagues a year with four divisions spanning all
+            abilities.
+          </div>
+          <div className="buttons-container">
+            <div className="button accent">Find out more</div>
+            <div className="button">Latest results</div>
+          </div>
+        </div>
+      </div>
+      <div className="line div1"> </div>
+      <div className="line div2"> </div>
+      <div className="line div3"> </div>
+      <div className="line div4"> </div>
+      <div className="line div5"> </div>
+      <div className="line div6"> </div>
+      <div className="line div7"> </div>
+      <div className="line div8"> </div>
+      <div className="line div9"> </div>
+      <div className="line div10"> </div>
+    </div>
+  );
+}
+
+export default CourtHero;
