69bc6d330a5bbc5137e0ecf1905345abad7cd751
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-13 15:15:55 +0100

    animations

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import "../styles/Public/styles.scss";
+import { motion } from "framer-motion";
 import { useLenis } from "@studio-freight/react-lenis";
 import CourtHero from "../components/CourtHero";
 import LatestResults from "../components/PublicData/LatestResults";
@@ -12,14 +13,32 @@ export default function Home() {
   return (
     <main className="main-layout-client">
       <CourtHero />
-      <div className="image-hero"></div>
+      <motion.div
+        className="image-hero"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.2, ease: "easeIn" }}
+      >
+        <motion.img
+          src="/1.png"
+          initial={{ opacity: 0, scale: 1.1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 1, duration: 0.45 }}
+        ></motion.img>
+      </motion.div>
 
       <div className="board">
-        <div className="main-text">
+        <motion.div
+          className="main-text"
+          initial={{ y: 20, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1, duration: 0.45 }}
+          viewport={{ once: true }}
+        >
           The league is designed to give match practice for external LTA
           competitions and to build confidence generally for all competitive
           play.
-        </div>
+        </motion.div>
       </div>
 
       <div className="latest-container">
--- app/components/CourtHero.jsx
+++ app/components/CourtHero.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import "../styles/Public/court.scss";
+import { motion } from "framer-motion";
 
 function CourtHero() {
   return (
-    <div className="court">
+    <motion.div
+      className="court"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2, ease: "easeIn" }}
+    >
       <div className="details ">
         <div className="text-container">
-          <div className="title">
+          <motion.div
+            className="title"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, duration: 0.5, ease: "easeIn" }}
+          >
             Highbury Tennis Club runs a thriving doubles league for its women
             members
-          </div>
-          <div className="subtitle">
+          </motion.div>
+          <motion.div
+            className="subtitle"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.6, duration: 0.4 }}
+          >
             There are two club leagues a year with four divisions spanning all
             abilities.
-          </div>
-          <div className="buttons-container">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+            className="buttons-container"
+          >
             <div className="button accent">Find out more</div>
             <div className="button">Latest results</div>
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="line div1"> </div>
@@ -30,7 +51,7 @@ function CourtHero() {
       <div className="line div8"> </div>
       <div className="line div9"> </div>
       <div className="line div10"> </div>
-    </div>
+    </motion.div>
   );
 }
 
