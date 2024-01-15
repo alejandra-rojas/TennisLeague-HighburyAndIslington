891e12b1c2a9a4e599e930cd6448845d3f56ea34
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-01-15 11:38:55 +0100

    Installed Hamo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,29 +1,35 @@
 "use client";
-import React from "react";
+import { useRect } from "@studio-freight/hamo";
+import { useRef } from "react";
 
 function Footer() {
+  const ref = useRef();
+  const [rectRef, rect] = useRect();
+
   return (
     <>
-      <div className="accent-container ">
-        <div className="accent">
-          <div className="text">
-            The next league will run from Mon 7th Aug to Mon 29th Jan 2024. Join
-            in!
+      <div ref={rectRef} className="footer">
+        <div className="accent-container ">
+          <div className="accent">
+            <div className="text">
+              The next league will run from Mon 7th Aug to Mon 29th Jan 2024.
+              Join in!
+            </div>
           </div>
         </div>
-      </div>
-      <footer>
-        <div className="baseline"></div>
-        <div className="center-line"></div>
+        <footer>
+          <div className="baseline"></div>
+          <div className="center-line"></div>
 
-        <div className="questions">
-          <h5>Any questions?</h5>
-          <p>
-            Get in touch with Sarah Mulligan, <br /> Co-ordinator of the Womens
-            League.
-          </p>
-        </div>
-      </footer>
+          <div className="questions">
+            <h5>Any questions?</h5>
+            <p>
+              Get in touch with Sarah Mulligan, <br /> Co-ordinator of the
+              Womens League.
+            </p>
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
