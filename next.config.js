ddbb2186d1c4b5b0cf695f7c302e91fb5cdf5ee1
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-02-02 13:40:49 +0100

    Removing "experimental.serverActions"

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -9,9 +10,6 @@ const nextConfig = {
       },
     ],
     domains: ["localhost", "cdn.sanity.io"],
-  },
-  experimental: {
-    serverActions: true,
   },
 };
 
