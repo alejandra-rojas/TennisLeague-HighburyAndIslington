d31344b229242b48d520fcd8973ee7a0ae8d8823
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-02-05 16:52:22 +0100

    Mobile stagger animation

--- app/components/MobileModal.jsx
+++ app/components/MobileModal.jsx
@@ -1,16 +1,12 @@
-import { useEffect, useRef } from "react";
 import { AnimatePresence, motion as m } from "framer-motion";
 import Link from "next/link";
-import MenuItem from "../components/MenuItem";
 import "../styles/Public/ModalMenu.scss";
-import Image from "next/image";
 
 const menu = [
   {
     title: "Homepage",
     slug: "/",
     description: "Back to ",
-    images: ["contact1.jpg", "contact2.jpg"],
   },
 
   {
@@ -23,52 +19,62 @@ const menu = [
     title: "Results",
     slug: "past-leagues",
     description: "Previous ",
-    images: ["contact1.jpg", "contact2.jpg"],
   },
 ];
 
-const animation = {
-  initial: { opacity: 0 },
-  enter: (index) => ({ opacity: 1, transition: { delay: 0.75 + index * 0.5 } }),
-  exit: { opacity: 0, duration: 0.75 },
+export const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+export const item = {
+  hidden: { y: "100%" },
+  show: { y: "0%", transition: { duration: 0.3 } },
 };
 
 function MobileModal({ handleClose }) {
   return (
     <div className="menu">
       <m.button
-        initial={{ y: 30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ delay: 1.25, duration: 0.45 }}
         onClick={handleClose}
       >
         close
       </m.button>
-      {/*  <nav className="menu--body">
+      <m.nav
+        className="menu--body"
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
+        <div className="overflow-hidden">
+          <m.a variants={item}>Menu</m.a>
+        </div>
+
         {menu.map((menuItem, index) => {
-          return <MenuItem data={menuItem} index={index} key={index} />;
+          return (
+            <div
+              key={index}
+              className="modalMenu-link overflow-hidden"
+              onClick={handleClose}
+            >
+              <Link href={`${menuItem.slug}`}>
+                <m.div variants={item}>{menuItem.title}</m.div>
+              </Link>
+            </div>
+          );
         })}
-      </nav> */}
+      </m.nav>
     </div>
   );
 }
 
 export default MobileModal;
-/*        <m.div
-            variants={animation}
-            animate="enter"
-            exit="exit"
-            initial="initial"
-            custom={index}
-            key={index}
-            className="menu__item"
-          >
-            <Link
-              href={menuItem.slug}
-              onClick={handleClose}
-              aria-label={`Go to the ${menuItem.title} page`}
-              className="menu__item-link"
-            >
-              {menuItem.title}
-            </Link>
-          </m.div> */
