6a4ebf114a089f63a74013b11591b7bdd1776146
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-02-06 10:15:15 +0100

    timing

--- app/components/public/TextCallout.jsx
+++ app/components/public/TextCallout.jsx
@@ -4,12 +4,12 @@ import { motion, useInView } from "framer-motion";
 
 const animation = {
   hidden: { opacity: 0, y: 30 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.45 } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.4 } },
 };
 
 function TextCallout({ data }) {
   const ref = useRef(null);
-  const isInView = useInView(ref, { amount: 0.45, once: true });
+  const isInView = useInView(ref, { amount: 0.4, once: true });
 
   return (
     <motion.div
