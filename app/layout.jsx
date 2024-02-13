3a4b685c7a7ef4835d9c071382039f539b61d13c
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-02-13 10:30:33 +0100

    Vercel Speed Insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import {
   Inter,
   Noto_Sans,
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
         <TanstackProvider>
           <SmoothScroll>{children}</SmoothScroll>
         </TanstackProvider>
-        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
