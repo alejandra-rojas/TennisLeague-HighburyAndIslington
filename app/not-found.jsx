df8129a19ac53adf330f42cf1f0c365e0578656d
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-02-18 19:58:55 +0100

    404 quick styling

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import "./styles/Root.scss";
+
+import { Barlow_Semi_Condensed } from "next/font/google";
+
+const barlow = Barlow_Semi_Condensed({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700", "900"],
+  variable: "--font-barlow",
+});
 
 export default function NotFound() {
   return (
-    <main>
-      <h2>We dropped the ball</h2>
-      <p>We could not find the page you were looking for.</p>
-      <p>
-        Go back to the <Link href="/">main page</Link>
-      </p>
+    <main className={`${barlow.variable} fourofour`}>
+      <div className="container">
+        <h2>OUT!</h2>
+        <p>We could not find the page you were looking for.</p>
+        <p className="back">
+          <Link href="/"> Go back to the main page</Link>
+        </p>
+      </div>
     </main>
   );
 }
