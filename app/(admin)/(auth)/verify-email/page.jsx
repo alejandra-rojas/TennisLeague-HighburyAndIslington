37f192df50c48de1844fdce89b57b90a0f433df1
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-03-08 19:15:14 +0100

    veify-email message styling

--- app/(admin)/(auth)/verify-email/page.jsx
+++ app/(admin)/(auth)/verify-email/page.jsx
@@ -3,8 +3,10 @@ import "../../../styles/Admin/Login.scss";
 
 export default function Verify() {
   return (
-    <main className="full-centered">
-      <h2>We have received your request</h2>
+    <main className="text-center h-screen translate-y-1/4">
+      <h2 className="uppercase font-bold text-xl pb-4 ">
+        We have received your request
+      </h2>
       <p>
         An email has been sent to your email provider. <br />
         Follow the provided link so we can complete your request.
