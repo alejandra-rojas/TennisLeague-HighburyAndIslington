7914d502ceb7eda2f1a5b08c6b262fb408dd8247
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-03-22 23:13:40 +0100

    Rules schema

--- sanity/schemas/rules-schema.js
+++ sanity/schemas/rules-schema.js
@@ -5,8 +5,14 @@ const rules = {
   fields: [
     { name: "Current", type: "Current League Dates", type: "string" },
     { name: "Midway", type: "Midway Date", type: "string" },
-    // { name: "callout", title: "Homepage Callout", type: "string" },
-    // { name: "next_league", title: "Next League Info", type: "string" },
+    { name: "format", title: "Format", type: "string" },
+    { name: "scoring", title: "Scoring", type: "string" },
+    { name: "injuries", title: "Injuries", type: "string" },
+    { name: "withdrawals", title: "Withdrawals", type: "string" },
+    { name: "winners", title: "Winners", type: "string" },
+    { name: "points", title: "Points", type: "string" },
+    { name: "bonusPoints", title: "Bonus Points", type: "string" },
+    { name: "challengers", title: "Challenger Matches", type: "string" },
   ],
 };
 
